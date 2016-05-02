c79a4a9c17b861f20ee7a52b7c489526a89cd771
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-05-02 17:17:39 +1000

    Callback now working!

--- WebContent/resources/js/openLayersExample.js
+++ WebContent/resources/js/openLayersExample.js
@@ -28,27 +28,31 @@ $(document).ready(function() {
 		})
 	});
 	
+	function dataCallback(returnedData) { // Implementation of the callback method. Never actually called except for when called back by success of JQuery's AJAX method.
+		console.log("Object is: ");
+		console.log(returnedData); // when the callback function is called on success, the the data is logged out!
+	}
 	
+	getJsonSensorData(dataCallback); // call AJAX function with callback function supplied as argument
 	
-	getJsonSensorData();
-	
-	function getJsonSensorData() {
+	function getJsonSensorData(callbackFunction) { // takes the callback function as a parameter
 		$.ajax({
 			method: 'GET',
 			// url: "http://localhost:8080/heimdall/get/waspmote/ES_B_08_422_7BDC/BAT/",
-			url: "http://localhost:8080/heimdall/get/waspmote/ES_B_08_422_7BDC/",
+			// url: "http://localhost:8080/heimdall/get/waspmote/ES_B_08_422_7BDC/",
 			// url: "http://localhost:8080/heimdall/get/weather/",
+			url: "http://localhost:8080/heimdall/get/sensors/all?level=5",
 			error: function() {
 				$('#info').html('<p>An error has occured</p>');
 			},
 			dataType: "json",
 			success: function(data) {
-				for (var i in data.subSensors) {
-					console.log(data.subSensors[i]);
-				}
+				//for (var i in data.subSensors) {
+				//	console.log(data.subSensors[i]);
+				//}
 				
 				$('#info').html('<p>Success!</p>');
-
+				callbackFunction(data); // executes the callback function with the data inside! :D 
 			}
 		})
 	}
